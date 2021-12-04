71adc6b1387b14d92f8c24fbdebf9d80786e2271
Jitka Badhan Nebeská <jitka.badhannebeska@vlmedia.cz>
2021-12-04 23:17:55 +0100

    Grid, components

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,5 @@
 module.exports = function(eleventyConfig) {
-	eleventyConfig.addPassthroughCopy("images");
-	eleventyConfig.addPassthroughCopy("css");
-	eleventyConfig.addPassthroughCopy("js");
+	eleventyConfig.addPassthroughCopy("assets");
 
   return {
     templateFormats: ["njk", "html", "md", "liquid"],
---
+++ _includes/partials/menu.njk
@@ -0,0 +1,3 @@
+<nav>
+    Menu
+</nav>
