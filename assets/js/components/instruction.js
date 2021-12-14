c502714523242e4307697b4e4b5b79ec2934e90e
Jitka Badhan Nebeská <jitka.badhannebeska@vlmedia.cz>
2021-12-14 12:48:31 +0100

    Rozbalení komponenty instrukcí

--- assets/js/components/instruction.js
+++ assets/js/components/instruction.js
@@ -4,9 +4,18 @@ const instruction = (elms) => {
         itemsToToggle.forEach( item => {
             const button = elm.querySelector('.instruction__heading');
             const arrow = elm.querySelector('.instruction__arrow');
+            const height = item.offsetHeight;
+
+            item.style.height = '0';            
             button.addEventListener('click', () => {
                 item.classList.toggle('instruction__body--hidden');
                 arrow.classList.toggle('instruction__arrow--up');
+
+                if (item.style.height == '0px' ) {
+                    item.style.height = height + 'px';
+                } else {
+                    item.style.height = '0';
+                }
             });
         } )
     });
